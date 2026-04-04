7a20180ce63971ba7338e431ea1f87c8c4ce54b8
Ozzus <serj.reintov228@gmail.com>
2026-04-04 13:21:05 +0300

    feat: add diasoft-registry application code

---
+++ src/main/java/com/diasoft/registry/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.diasoft.registry.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final AppProperties properties;
+
+    public WebConfig(AppProperties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(properties.http().allowedOrigins().toArray(String[]::new))
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
