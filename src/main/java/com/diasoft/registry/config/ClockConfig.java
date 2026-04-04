46a12014b4eab16a65500efb8bb598b5a0f81193
Ozzus <serj.reintov228@gmail.com>
2026-04-04 13:21:05 +0300

    feat: add diasoft-registry application code

---
+++ src/main/java/com/diasoft/registry/config/ClockConfig.java
@@ -0,0 +1,13 @@
+package com.diasoft.registry.config;
+
+import java.time.Clock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClockConfig {
+    @Bean
+    Clock clock() {
+        return Clock.systemUTC();
+    }
+}
