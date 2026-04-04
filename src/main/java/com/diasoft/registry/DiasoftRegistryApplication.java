36753b75e56bf26dd03e8f453615e0aa084c83e9
Ozzus <serj.reintov228@gmail.com>
2026-04-04 13:21:05 +0300

    feat: add diasoft-registry application code

---
+++ src/main/java/com/diasoft/registry/DiasoftRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.diasoft.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@ConfigurationPropertiesScan
+public class DiasoftRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiasoftRegistryApplication.class, args);
+    }
+}
