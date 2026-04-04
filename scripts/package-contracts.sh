e38721906d0fb38feaa68616b5c8134cea218641
Ozzus <serj.reintov228@gmail.com>
2026-04-04 13:21:05 +0300

    chore: add diasoft-registry runtime helpers

---
+++ scripts/package-contracts.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+output_dir="${1:-build/contracts}"
+version="${2:-dev}"
+package_name="${CONTRACTS_PACKAGE_NAME:-registry-contracts}"
+
+mkdir -p "$output_dir/$package_name/api/openapi" "$output_dir/$package_name/contracts"
+cp api/openapi/internal.yaml "$output_dir/$package_name/api/openapi/internal.yaml"
+cp -R contracts/kafka "$output_dir/$package_name/contracts/kafka"
+
+tarball_path="$output_dir/$package_name.tar.gz"
+tar -C "$output_dir" -czf "$tarball_path" "$package_name"
+
+echo "Packaged $package_name version $version at $tarball_path"
